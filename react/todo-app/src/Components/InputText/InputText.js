d23c5444f4863c94aa46e1698a36748d6a2ef12e
Caio Andrade <caiotdandrade@gmail.com>
2019-08-05 08:34:22 -0300

    Adds useEffect hook and replaces wrapping div for a form
    - useEffect hook added in order to clean up input value after task is added
    - replaces div for a form because it is more semantical, and this allows
        me to submit the input value either by pressing the '+' button, or
        pressing enter inside the form.
    
    Signed-off-by: Caio Andrade <caiotdandrade@gmail.com>

--- react/todo-app/src/Components/InputText/InputText.js
+++ react/todo-app/src/Components/InputText/InputText.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const InputText = ( props ) => (
-    <div>
-        <input placeholder="What else?" />
-        <button onClick={props.clicked}> + </button>
-    </div>
-)
+
+const InputText = ( props ) => {
+    
+    useEffect( () => {
+        const input = document.querySelector('input');
+        input.value = '';
+    });
+
+    return (
+        <form>
+            <input placeholder="What else?" />
+            <button onClick={props.clicked} type="submit"> + </button>
+        </form>
+    )
+}
 
 export default InputText;
