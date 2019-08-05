c29cd4a47a72bd73bf79ba0b87e2c320be38471f
Caio Andrade <caiotdandrade@gmail.com>
2019-08-05 16:19:49 -0300

    Includes Header.js and alter state
    - Includes new component to display title and how many tasks are there
    - State now stores a object instead of just text
        - The object stores the text and if the task is done or not
        (false by default)
    
    Signed-off-by: Caio Andrade <caiotdandrade@gmail.com>

--- react/todo-app/src/Components/toDo/ToDo.js
+++ react/todo-app/src/Components/toDo/ToDo.js
@@ -1,15 +1,20 @@
 import React, { useState, Fragment } from 'react';
 import ToDoItem from '../ToDoItem/ToDoItem';
 import InputText from '../InputText/InputText';
+import Header from '../Header/Header'
 import Style from './ToDo.module.css';
+
+// TODO : mover o estado pro App.js
 
 const ToDo = (props) => {
 
-    const [list, updateList] = useState(['Limpar a casa', 'Estudar React']);
+    const [list, updateList] = useState([{value: 'Study React', done: false}]);
 
     const removeTaskHandler = (index) => {
+        console.log('To sendo chamado!');
         let newList = [...list];
-        newList.splice(index, 1);
+        console.log(newList[0]);
+        newList[index].done = !newList[index].done;
         updateList(newList);
     }
 
@@ -19,26 +24,30 @@ const ToDo = (props) => {
 
         if (text.length > 0) {
             const newList = [...list];
-            newList.push(text);
+            newList.push({value: text, done: false});
             updateList(newList);
         }
     }
 
     let toDo = list.map( (task, index) => 
         <ToDoItem 
-            item={task}
+            item={task.value}
             clicked={removeTaskHandler}
             key={index}
             index={index}
+            done={task.done}
         />
     )
     return (
         <Fragment>
+            <Header
+              title="ToDo"
+              count={list.length}
+            />
             <div className={Style.listContainer}>
-                <h1>ToDo list</h1>  
-                <ol>
+                <ul className={Style.list}>
                     {toDo}
-                </ol>
+                </ul>
                 <InputText 
                     clicked={addTaskHandler}
                 />
