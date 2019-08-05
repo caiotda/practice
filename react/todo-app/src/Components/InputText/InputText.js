6f15ac320063e9d296d73dbeaef963cd6c787b12
Caio Andrade <caiotdandrade@gmail.com>
2019-08-05 16:17:28 -0300

    Adds UseEffect to InputText.js and style
    - UseEffect is used in order to clear input value after task is added
    - Adds style to input
    
    Signed-off-by: Caio Andrade <caiotdandrade@gmail.com>

--- react/todo-app/src/Components/InputText/InputText.js
+++ react/todo-app/src/Components/InputText/InputText.js
@@ -1,16 +1,17 @@
 import React, { useEffect } from 'react';
+import Style from './InputText.module.css'
 
 
 const InputText = ( props ) => {
     
     useEffect( () => {
-        const input = document.querySelector('input');
+        const input = document.querySelector('#clear');
         input.value = '';
     });
 
     return (
-        <form>
-            <input placeholder="What else?" />
+        <form className={Style.form}>
+            <input type="text" id="clear" placeholder="What else?" className={Style.input}/>
             <button onClick={props.clicked} type="submit"> + </button>
         </form>
     )
