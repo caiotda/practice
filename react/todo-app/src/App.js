b532ceb53183410c9267578eab1ad0c30887b5f0
Caio Andrade <caiotdandrade@gmail.com>
2019-08-15 12:53:05 -0300

    Adds new useState hook and methods to remove done tasks
    - adds doneList state : Adds state that keeps track of tasks that are
        done.
    
    - adds methods to remove done tasks : adds method that removes all tasks
        in toDoLists that are target as done and moves them to doneList
    
    Signed-off-by: Caio Andrade <caiotdandrade@gmail.com>

--- react/todo-app/src/App.js
+++ react/todo-app/src/App.js
@@ -6,20 +6,34 @@ import './App.css';
 
 function App() {
 
-  const [list, updateList] = useState([{value: 'Study React', done: false}]);
+  const [toDoList, updateToDoList] = useState([{value: 'Study React', done: false}]);
+  const [doneList, updateDoneList] = useState([]);
 
+
+  const cleanDoneTasks = () => {
+    const clone = [...toDoList];
+    const newTodo = clone.filter(item => !item.done);
+    const newDoneTasks = clone.filter(item => item.done); // preciso preencher com as tarefas que ja estavam prontas, puxo do estado
+    const doneTasks = [...doneList].concat(newDoneTasks);
+    console.log(newTodo, doneTasks);
+
+    updateToDoList(newTodo);
+    updateDoneList(doneTasks);
+  }
   return (
     <main className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
       <Header 
         title="toDo"
-        count={list.length}
+        count={toDoList.length}
       />
       <ToDo 
-        list={list}
-        updateList={updateList}
+        list={toDoList}
+        updateList={updateToDoList}
+        cleanDoneTasks={cleanDoneTasks}
       />
+
 
       </header>
     </main>
